44ad2f525dfc3e5caf6c051ed5bad0f8cd714457
rainbowbear <113357212+rainbow96bear@users.noreply.github.com>
2024-12-27 15:09:27 +0900

    update: main.go 기본 설정 추가 및 주석 설명 추가

--- server/chat/main.go
+++ server/chat/main.go
@@ -23,12 +23,25 @@ func main() {
 		port = defaultPort
 	}
 
+	/*
+	GraphQL 요청을 처리하는 핸들러 생성
+ 	graph.Config의 resolver 지정
+ 	*/
 	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &resolver.Resolver{}}))
 
+	// CORS 설정을 확인하기 위한 Options 요청 처리 설정
 	srv.AddTransport(transport.Options{})
+	// 클라이언트가 HTTP GET 요청을 통한 처리 허용 설정
 	srv.AddTransport(transport.GET{})
+	// 클라이언트가 HTTP POST 요청을 통한 처리 허용 설정
 	srv.AddTransport(transport.POST{})
-
+	// 클라이언트가 WebSocket을 통한 지속적 연결 설정
+	srv.AddTransport(transport.Websocket{})
+	// GraphQL API에서 파일 업로드 지원 설정
+	srv.AddTransport(transport.MulipartForm{})
+	// 클라이언트의 과도한 요청 방지
+	srv.Use(extension.FixedComplexityLimit{ComplexityLimit: 100})
+	
 	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
 
 	srv.Use(extension.Introspection{})
