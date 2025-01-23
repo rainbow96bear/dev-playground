6397b7196e6eadb6e9da56d02e4422e730c0e09b
rainbow96bear <rainbow96bear@gmail.com>
2025-01-23 17:57:37 +0900

    update : symbol css

--- client/maple_hub/src/lib/components/characterInfo/symbols/arcane/Arcane.svelte
+++ client/maple_hub/src/lib/components/characterInfo/symbols/arcane/Arcane.svelte
@@ -1,13 +1,18 @@
 <script>
-    import "./Symbol.css"
+    import "./Arcane.css"
 
     export let symbol_icon;
     export let symbol_level;
 </script>
 
-<div id="symbol_container">
+<div id="symbol_box">
     <div class="symbol_img_box">
         <img src={symbol_icon} alt="symbol_icon"/>
     </div>
     <div class="symbol_level">{symbol_level}</div>
+    <div class="gauge_box">
+        <div class="gague_base">
+            <div class="gauge_bar" style="width: {50}%"></div>
+        </div>
+    </div>
 </div>
---
+++ client/maple_hub/src/lib/components/characterInfo/symbols/authenitc/Authentic.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "./Arcane.css"
+</script>
+
+<div class="hexagon-container">
+    <div class="hexagon"></div>
+</div>
