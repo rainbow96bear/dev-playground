5ef05386ad88079583c9b3df344b4266deab0f83
rainbowbear <rainbow96bear@gmail.com>
2025-03-25 23:31:15 +0900

    update : netlify command

--- client/maple_hub/src/lib/components/equipmentIcon/EquipmentIcon.svelte
+++ client/maple_hub/src/lib/components/equipmentIcon/EquipmentIcon.svelte
@@ -6,7 +6,7 @@
     import "./EquipmentIcon.css"
 </script>
 
-<div role="button" tabindex="0" onclick={() => func()} class="img_detail_box center {PotentialOptionToEng[item?.potential_option_grade]}_img_border {func==undefined ? "" : "cursor_pointer"}">
+<div role="button" tabindex="0" onclick={() => func()} onkeydown={(e) => e.key === 'Enter' && func()} class="img_detail_box center {PotentialOptionToEng[item?.potential_option_grade]}_img_border {func==undefined ? "" : "cursor_pointer"}">
     {#if item?.item_icon}
         <img class="img_detail" src={item.item_icon} alt="item img"/>
     {/if}
