f34e824a3449e8b62f5c194bef844e85ab98e22e
rainbowbear <rainbow96bear@gmail.com>
2025-03-29 13:42:54 +0900

    add : patch notes page

---
+++ client/maple_hub/src/routes/(app)/patch_notes/[version]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "./+page.css";
+  
+  import { marked } from 'marked';  // marked 라이브러리 가져오기
+  export let data;
+
+  // Markdown을 HTML로 변환
+  let contentHtml = marked(data.content);
+</script>
+
+{#if contentHtml}
+  <div class="content" >{@html contentHtml}</div>
+{:else}
+  <p class="no-content">패치 노트가 없습니다.</p>
+{/if}
