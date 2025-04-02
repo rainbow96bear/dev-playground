ea469791dcb6e7beb92b79316b2e0de26f782595
rainbowbear <rainbow96bear@gmail.com>
2025-04-02 20:45:11 +0900

    fix : robots의 sitemap 주소

--- client/maple_hub/src/routes/robots.txt/+server.ts
+++ client/maple_hub/src/routes/robots.txt/+server.ts
@@ -2,7 +2,7 @@ export const GET = async () => {
 	return new Response(
 		`User-agent: *
 Allow: /
-Sitemap: https://example.com/sitemap.xml`,
+Sitemap: https://maplebox.netlify.app/sitemap.xml`,
 		{
 			headers: {
 				'Content-Type': 'text/plain'
