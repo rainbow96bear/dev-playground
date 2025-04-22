15e144b57e08f409e1f0cdf09861e0b6accaee29
rainbowbear <rainbow96bear@gmail.com>
2025-04-22 21:05:02 +0900

    test : seo 적합합

--- client/maple_hub/src/lib/components/characterInfo/CharacterInfo.svelte
+++ client/maple_hub/src/lib/components/characterInfo/CharacterInfo.svelte
@@ -27,59 +27,59 @@
     if (intervalId) clearInterval(intervalId);
   });
 
-  const toggleMotion = () => {
-    characterWalkMotion.set((motionNum === 2 ? 3 : 2));
-  };
-
   const selectComponent = (componentName: string) => {
     selectedComponent = componentName;
   };
 </script>
 
-{#if isDataReady}
-  {#if currentInfo }
-    <div class="character_info_box">
-      <div class="main_info display_flex">
-        <img src={currentInfo.character_image} alt="캐릭터 이미지" />
-        <div class="text_main_info">
-          <div id="nickname">{currentInfo.character_name}</div>
-          <div>{currentInfo.character_class} | Lv.{currentInfo.character_level}</div>
-          <div>{currentInfo.world_name} / {currentInfo.character_guild_name || '길드 없음'}</div>
-        </div>
-      </div>
-      <div>
-        <div id="function_button_box">
-          <button
-            class:selected={selectedComponent === 'EquippedItems'}
-            on:click={() => selectComponent('EquippedItems')}
-          >
-            장비
-          </button>
-          <button
-            class:selected={selectedComponent === 'HexaSkills'}
-            on:click={() => selectComponent('HexaSkills')}
-          >
-            헥사 스킬
-          </button>
-          <button
-            class:selected={selectedComponent === 'Symbols'}
-            on:click={() => selectComponent('Symbols')}
-          >
-            심볼
-          </button>
-        </div>
-        <div id="sub_info_box">
-          {#if selectedComponent === 'EquippedItems'}
-            <EquippedItems />
-          {/if}
-          {#if selectedComponent === 'HexaSkills'}
-            <HexaSkills />
-          {/if}
-          {#if selectedComponent === 'Symbols'}
-            <Symbols />
-          {/if}
-        </div>
+{#if isDataReady && currentInfo}
+  <div class="character_info_box">
+    <div class="main_info display_flex">
+      <img src={currentInfo.character_image} alt="캐릭터 이미지" />
+      <div class="text_main_info">
+        <div id="nickname">{currentInfo.character_name}</div>
+        <div>{currentInfo.character_class} | Lv.{currentInfo.character_level}</div>
+        <div>{currentInfo.world_name} / {currentInfo.character_guild_name || '길드 없음'}</div>
       </div>
     </div>
-  {/if}
+
+    <div>
+      <div id="function_button_box">
+        <button
+          class:selected={selectedComponent === 'EquippedItems'}
+          on:click={() => selectComponent('EquippedItems')}
+        >
+          장비
+        </button>
+        <button
+          class:selected={selectedComponent === 'HexaSkills'}
+          on:click={() => selectComponent('HexaSkills')}
+        >
+          헥사 스킬
+        </button>
+        <button
+          class:selected={selectedComponent === 'Symbols'}
+          on:click={() => selectComponent('Symbols')}
+        >
+          심볼
+        </button>
+      </div>
+
+      <div id="sub_info_box">
+        <!-- EquippedItems는 항상 마운트되며 보이기만 조건으로 처리 -->
+        <div style="display: {selectedComponent === 'EquippedItems' ? 'block' : 'none'}">
+          <EquippedItems />
+        </div>
+
+        <!-- 나머지는 조건부 렌더링 -->
+        {#if selectedComponent === 'HexaSkills'}
+          <HexaSkills />
+        {/if}
+
+        {#if selectedComponent === 'Symbols'}
+          <Symbols />
+        {/if}
+      </div>
+    </div>
+  </div>
 {/if}
